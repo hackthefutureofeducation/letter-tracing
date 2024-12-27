258ed52282ea2f62655eb01581e9aa18e8e4eec0
Bashamega <adambashaahmednaji@gmail.com>
2024-12-27 10:43:59 +0200

    mobile

---
+++ src/components/MobileFlip.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import '../MobileFlip.css'
+
+export const MobileFlip: React.FC = () => {
+    const [isPortrait, setIsPortrait] = useState<boolean>(true);
+
+    const handleOrientationChange = () => {
+        setIsPortrait(window.innerHeight > window.innerWidth);
+    };
+
+    useEffect(() => {
+        window.addEventListener('resize', handleOrientationChange);
+        handleOrientationChange(); // Check initial orientation
+
+        return () => {
+            window.removeEventListener('resize', handleOrientationChange);
+        };
+    }, []);
+
+    if (isPortrait) {
+        return (
+            <div id="warning-message">
+                <div className="phone">
+                </div>
+                <div className="message" dir="rtl">
+                    يرجى تدوير جهازك!
+                </div>
+            </div>
+        );
+    }
+
+    return null; // Do not render anything if the device is flipped
+};
